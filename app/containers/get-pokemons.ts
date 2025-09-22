ff114a077dba16aec6a8da995b6e4ba779dbf74d
Hacklaender <nortonhw@gmail.com>
2025-09-21 21:28:33 -0300

    feat: adicionando as imagens faltantes e tema responsivel

--- app/containers/get-pokemons.ts
+++ app/containers/get-pokemons.ts
@@ -16,6 +16,16 @@ export function useGetPokemons() {
   );
 
   useEffect(() => {
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        setIsDarkMode(mediaQuery.matches);
+
+        const handler = (e: MediaQueryListEvent) => setIsDarkMode(e.matches);
+        mediaQuery.addEventListener("change", handler);
+
+        return () => mediaQuery.removeEventListener("change", handler);
+    }, []);
+
+  useEffect(() => {
     fetch("/pokemons.json")
       .then((res) => res.json())
       .then((data: PokemonsDTO[]) => setPokemons(data));
